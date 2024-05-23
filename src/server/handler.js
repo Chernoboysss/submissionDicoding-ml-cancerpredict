fa216fb30797a10310c4adde452a8c22331d55a7
Chernoboysss <Mhmdfadillah1@gmail.com>
2024-05-23 15:27:25 +0700

    fix -update

--- src/server/handler.js
+++ src/server/handler.js
@@ -4,42 +4,58 @@ const InputError = require('../exceptions/InputError');
 const storeData = require('../services/storeData');
 
 async function postPredictHandler(request, h) {
-    const { image } = request.payload;
+  const { image } = request.payload;
 
-    try {
-        const { model } = request.server.app;
-        const { label, suggestion } = await predictClassification(model, image);
-        const id = crypto.randomUUID();
-        const createdAt = new Date().toISOString();
+  try {
+      const { model } = request.server.app;
+      const { confidenceScore,label, suggestion } = await predictClassification(model, image);
+      const id = crypto.randomUUID();
+      const createdAt = new Date().toISOString();
 
-        const data = {
-            "id": id,
-            "result": label,
-            "suggestion": suggestion,
-            "createdAt": createdAt
-        };
+      const data = {
+          "id": id,
+          "result": label,
+          "suggestion": suggestion,
+          "createdAt": createdAt,
+          "confidenceScore": confidenceScore,
+      };
 
-        await storeData(id, data);
-        
-        const response = h.response({
-            status: 'success',
-            message: 'Model is predicted successfully.',
-            data
-        });
-        response.code(201);
-        return response;
-    } catch (error) {
-        if (error instanceof InputError) {
-            return h.response({
-                status: 'fail',
-                message: error.message
-            }).code(400);
-        }
-        return h.response({
-            status: 'error',
-            message: 'Terjadi kesalahan dalam melakukan prediksi.'
-        }).code(500);
-    }
+      // await storeData(id, data);
+      
+      // if (confidenceScore <= 10){
+      //   const response = h.response({
+      //     status: 'fail',
+      //     message : 'Terjadi kesalahan dalam melakukan prediksi'
+      //   });
+      //   response.code(400);
+
+      //   return response ;
+      // }
+
+
+      const response = h.response({
+          status: 'success',
+          message: 'Model is predicted successfully',
+          data
+      });
+      response.code(201);
+      return response;
+
+
+  } catch (error) {
+      if (error instanceof InputError) {
+          // Ubah status menjadi 400 untuk kesalahan input
+          return h.response({
+              status: 'fail',
+              message: error.message
+          }).code(400);
+      }
+      // Ubah status menjadi 400 untuk kesalahan prediksi
+      return h.response({
+          status: 'fail',
+          message: 'Terjadi kesalahan dalam melakukan prediksi.'
+      }).code(400);
+  }
 }
 
 module.exports = postPredictHandler;
--- src/server/server.js
+++ src/server/server.js
@@ -8,7 +8,7 @@ const PredictionError = require('../exceptions/PredictionError');
 (async () => {
     const server = Hapi.server({
         port: 3000,
-        host: '0.0.0.0',
+        host: 'localhost',
         routes: {
             cors: {
               origin: ['*'],
